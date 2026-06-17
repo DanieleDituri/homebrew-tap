0bb639e58e1ec4b9f50c3d3c57da5f4e4a757edf
daniele.dituri <daniele.dituri@mediolanum.it>
2026-06-17 10:15:58 +0200

    countdownapp 0.3.2

--- Casks/countdownapp.rb
+++ Casks/countdownapp.rb
@@ -1,8 +1,8 @@
 cask "countdownapp" do
-  version "0.3.1"
-  sha256 "639b11f838e59195cd173eff1760d4c5bb00b009e592ea88dd843e4402445366"
+  version "0.3.2"
+  sha256 "d3d4d192074d8ccb3feacb20ca3bf67ce64ae97622188b05339f8aec1a61adcd"
 
-  url "https://github.com/DanieleDituri/countdown_app/releases/download/v0.3.1/CountdownApp.dmg"
+  url "https://github.com/DanieleDituri/countdown_app/releases/download/v0.3.2/CountdownApp.dmg"
   name "CountdownApp"
   desc "Countdown timer for your events with menu bar and desktop widgets"
   homepage "https://github.com/DanieleDituri/countdown_app"
